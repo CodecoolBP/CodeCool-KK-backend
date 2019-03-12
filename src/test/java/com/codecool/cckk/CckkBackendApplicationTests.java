10ef8b32b865874b393b71f91e191c147247e102
Akos Kolloros <kollorosa@gmail.com>
2019-03-12 10:01:54 +0100

    initial backend /w base springboot

---
+++ src/test/java/com/codecool/cckk/CckkBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.codecool.cckk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CckkBackendApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
