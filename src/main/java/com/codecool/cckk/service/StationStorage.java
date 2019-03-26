9037ae0c7dbec9fd67a5b6f7834ca432842aa67c
gaborKriger <regirck@gmail.com>
2019-03-26 18:56:21 +0100

    post mapping station save to the database

--- src/main/java/com/codecool/cckk/service/StationStorage.java
+++ src/main/java/com/codecool/cckk/service/StationStorage.java
@@ -2,7 +2,9 @@ package com.codecool.cckk.service;
 
 import com.codecool.cckk.model.ReturnMessage;
 import com.codecool.cckk.model.station.Station;
+import com.codecool.cckk.repository.StationRepository;
 import lombok.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -15,12 +17,15 @@ import java.util.List;
 @Builder
 public class StationStorage {
 
+    @Autowired
+    private StationRepository stationRepository;
+
     @Singular
     private List<Station> stations = new ArrayList<>();
 
     public ReturnMessage addStation(Station station) {
-        //TODO add new station
         stations.add(station);
+        stationRepository.save(station);
         //TODO check station is exists
         return new ReturnMessage(true, "Add new station success!");
     }
