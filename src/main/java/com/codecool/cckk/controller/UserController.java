879514b1d54a8aa33c156494be67e02499593dfd
gaborkallos <gaborkallos@gmail.com>
2019-03-27 11:20:37 +0100

    userController merged

--- src/main/java/com/codecool/cckk/controller/UserController.java
+++ src/main/java/com/codecool/cckk/controller/UserController.java
@@ -3,8 +3,6 @@ package com.codecool.cckk.controller;
 import com.codecool.cckk.model.CckkUser;
 import com.codecool.cckk.model.ReturnMessage;
 import com.codecool.cckk.repository.UserRepository;
-import com.codecool.cckk.service.UserStorage;
-import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,9 +13,6 @@ import java.util.List;
 @RequestMapping("/user")
 @CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
-
-    @Autowired
-    private UserStorage userStorage;
 
     @Autowired
     private UserRepository userRepository;
