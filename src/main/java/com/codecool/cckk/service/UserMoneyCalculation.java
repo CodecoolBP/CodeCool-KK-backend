cfbf59bfc08aa65499a0e93e8343fab961ebe089
zsombor <apathy.zsombor@gmail.com>
2019-03-27 17:20:28 +0100

    new classes created for business logic

---
+++ src/main/java/com/codecool/cckk/service/UserMoneyCalculation.java
@@ -0,0 +1,30 @@
+package com.codecool.cckk.service;
+
+import com.codecool.cckk.model.CckkUser;
+import com.codecool.cckk.model.ReturnMessage;
+import com.codecool.cckk.repository.TripRepository;
+import com.codecool.cckk.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/moneycalc")
+public class UserMoneyCalculation {
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    TripRepository tripRepository;
+
+    @PostMapping("/query")
+    public ReturnMessage checkIfUserCanTravel(@RequestBody CckkUser user) {
+        //TODO: user has purchased pass
+        //TODO: user has traveled enough to acquire pass
+        //TODO: user has money
+
+
+        return new ReturnMessage(true, "Nothing happened yet");
+    }
+
+}
---
+++ src/test/java/com/codecool/cckk/service/UserMoneyCalculationTest.java
@@ -0,0 +1,16 @@
+package com.codecool.cckk.service;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class UserMoneyCalculationTest {
+
+
+}
