4bdc66c1b5e77339f987a757e715b9dd107b3bea
gaborkallos <gaborkallos@gmail.com>
2019-03-27 14:15:13 +0100

    hashedpassword changed to password

--- src/main/java/com/codecool/cckk/controller/UserController.java
+++ src/main/java/com/codecool/cckk/controller/UserController.java
@@ -24,18 +24,33 @@ public class UserController {
     }
 
     @PostMapping("/add")
-    public ReturnMessage addUser(@RequestBody @Valid CckkUser cckkUser) {
-        List<CckkUser> users = this.userRepository.findAll();
+    public ReturnMessage addUser(@RequestBody @Valid CckkUser newUser) {
+        List<CckkUser> users = getUsers();
         for (CckkUser user : users) {
-            if (user.getEmail().equals(cckkUser.getEmail())) {
+            if (user.getEmail().equals(newUser.getEmail())) {
                 return new ReturnMessage(false, "Email address is already taken!");
             }
         }
-        this.userRepository.save(cckkUser);
+        this.userRepository.save(newUser);
+        loginUser(newUser);
         return new ReturnMessage(true, "Registration is success!");
 
     }
 
+    @GetMapping("/login")
+    public ReturnMessage loginUser(@RequestBody CckkUser loginUser){
+        List<CckkUser> users = getUsers();
+        for (CckkUser user : users){
+            if (user.getEmail().equals(loginUser.getEmail())){
+                if(user.getPassword().equals(loginUser.getPassword())){
+                    return new ReturnMessage(true, "Login successful");
+                }else{
+                    return new ReturnMessage(false, "Password is incorrect");
+                }
+            }
+        }
+        return new ReturnMessage(false, "Email address is incorrect, no user found!");
+    }
 
     //TODO: put mapping
     //TODO: rest routing
