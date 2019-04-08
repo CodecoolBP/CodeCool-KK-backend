80eccc655663247ae366e8b90cdbd7668f908846
gaborKriger <regirck@gmail.com>
2019-04-08 15:01:44 +0200

    error

--- src/main/java/com/codecool/cckk/repository/TripRepository.java
+++ src/main/java/com/codecool/cckk/repository/TripRepository.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public interface TripRepository extends JpaRepository<Trip, Long> {
 
