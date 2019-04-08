02ee3651c7b8306a6f7f1be59eb5299f85921e7a
gaborKriger <regirck@gmail.com>
2019-04-08 14:58:25 +0200

    refactor TripController

--- src/main/java/com/codecool/cckk/controller/TripController.java
+++ src/main/java/com/codecool/cckk/controller/TripController.java
@@ -30,23 +30,8 @@ public class TripController {
 
     @GetMapping("/{email}/history")
     public List<Trip> getTrips(@PathVariable("email") String email){
-        System.out.println(email);
         CckkUser user = userRepository.findUserByEmail(email);
-        System.out.println(user.getFirstName() + " " + user.getLastName());
-        System.out.println(user.getEmail());
-
-        System.out.println(user.getId());
-
-
         ArrayList<Trip> trips = tripRepository.findAllByUserId(user);
-
-        for (Trip trip : trips) {
-            System.out.println(trip.getFromStation());
-        }
-
-        if (trips.isEmpty()) {
-            return null;
-        }
         return trips;
     }
 }
